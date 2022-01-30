798e634a17fe722c7914500e6f0552c376aa06f2
Younext19 <youneshaddam9@gmail.com>
2022-01-30 03:13:38 +0100

    adding navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => (
         <a href="#home">Home</a>
       </li>
       <li className="p__opensans">
-        <a href="#about">About us</a>
+        <a href="#about">About</a>
       </li>
       <li className="p__opensans">
         <a href="#menu">Menu</a>
@@ -27,6 +27,35 @@ const Navbar = () => (
         <a href="#contact">Contact</a>
       </li>
     </ul>
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">
+        Login / Register
+      </a>
+      <div></div>
+      <a href="/" className="p__opensans">
+        Book Table{" "}
+      </a>
+    </div>
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
+      <ul className="app__navbar-smallscreen-links">
+        <li className="p__opensans">
+          <a href="#home">Home</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#about">About</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#menu">Menu</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#awards">Awards</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
