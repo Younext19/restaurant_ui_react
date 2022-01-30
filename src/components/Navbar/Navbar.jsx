1be09e2fcaed9bc440a7b97c7aa54a6df600151f
Younext19 <youneshaddam9@gmail.com>
2022-01-30 20:11:39 +0100

    header ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,44 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { MdOutlineResaturantMenu } from "react-icons/md";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
 import images from "../../constants/images";
 import "./Navbar.css";
 
-const Navbar = () => (
-  <div className="app__navbar">
-    <div className="app__navbar-logo">
-      <img src={images.gericht} alt="app logo" />
-    </div>
-    <ul className="app__navbar-links">
-      <li className="p__opensans">
-        <a href="#home">Home</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#about">About</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#menu">Menu</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#awards">Awards</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#contact">Contact</a>
-      </li>
-    </ul>
-    <div className="app__navbar-login">
-      <a href="#login" className="p__opensans">
-        Login / Register
-      </a>
-      <div></div>
-      <a href="/" className="p__opensans">
-        Book Table{" "}
-      </a>
-    </div>
-    <div className="app__navbar-smallscreen">
-      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
-      <ul className="app__navbar-smallscreen-links">
+const Navbar = () => {
+  const [ToggleMenu, setToggleMenu] = useState(false);
+  return (
+    <div className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gericht} alt="app logo" />
+      </div>
+      <ul className="app__navbar-links">
         <li className="p__opensans">
           <a href="#home">Home</a>
         </li>
@@ -55,8 +29,54 @@ const Navbar = () => (
           <a href="#contact">Contact</a>
         </li>
       </ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          Login / Register
+        </a>
+        <div></div>
+        <a href="/" className="p__opensans">
+          Book Table{" "}
+        </a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => {
+            setToggleMenu(true);
+          }}
+        />
+        {ToggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => {
+                setToggleMenu(false);
+              }}
+            />
+            <ul className="app__navbar-smallscreen-links">
+              <li className="p__opensans">
+                <a href="#home">Home</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#about">About</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#menu">Menu</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#awards">Awards</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
-import './Header.css';
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1"> The key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec imperdiet
+        tortor dolor, a aliquet sem fringilla eget. Mauris ac ligula tellus.
+        Integer pellentesque enim nec purus tristique euismod vel eu risus. Ut
+        nec orci lacinia, scelerisque lacus quis, faucibus purus.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
