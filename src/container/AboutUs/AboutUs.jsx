849a0c474ed34389b9fc955aecc10a4a552ea646
Younext19 <youneshaddam9@gmail.com>
2022-01-31 20:25:38 +0100

    add menu of restaurant and about us sections

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
-
-import './AboutUs.css';
+import React from "react";
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G letter" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img className="spoon__img" src={images.spoon} alt="about_spoon" />
+        <p className="p__opensans">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book
+        </p>
+        <button type="button" className="custom__button">
+          Know more
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img className="spoon__img" src={images.spoon} alt="about_spoon" />
+        <p className="p__opensans">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book
+        </p>
+        <button type="button" className="custom__button">
+          Know more
+        </button>
+      </div>
+    </div>
   </div>
 );
 
